1ff7876dd0188759213e205a6dd878bd3bb31bb5
mahdifahumie10 <mahdifahumie@gmail.com>
2025-09-27 18:25:31 +0800

    Delete OWSBSystem/FinanceManager directory

--- OWSBSystem/FinanceManager/src/com/owsb/system/main/Main.java
+++
@@ -1,9 +0,0 @@
-package com.owsb.system.main;
-
-import com.owsb.system.gui.LoginGUI;
-
-public class Main {
-    public static void main(String[] args) {
-        new LoginGUI().setVisible(true);
-    }
-}
